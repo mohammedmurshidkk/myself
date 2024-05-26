ec0f4e4a2445d8e674f4cf2cca09e6965cf97b2f
mohammedmurshidkk <muhammedmurshid43@gmail.com>
2024-05-26 13:41:46 +0530

    CV Enhancment

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,26 @@
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+  $(".navbar .nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
 
-        if (this.hash !== "") {
+      var hash = this.hash;
 
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 });
 
 // navbar toggle
-$('#nav-toggle').click(function(){
-    $(this).toggleClass('is-active')
-    $('ul.nav').toggleClass('show');
+$("#nav-toggle").click(function () {
+  $(this).toggleClass("is-active");
+  $("ul.nav").toggleClass("show");
 });
